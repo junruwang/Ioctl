df9088757dcce9d31ff5c998ca296388fa938c23
allen <403033452@qq.com>
2018-01-11 17:30:02 +0800

    修改界面，添加一个Spinner，并完成五个管脚的测试

--- app/src/main/java/com/guoguang/ioctl/MainActivity.java
+++ app/src/main/java/com/guoguang/ioctl/MainActivity.java
@@ -4,7 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -12,9 +16,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private IoctlExec ioctlExec;
     private static final String TAG="iocjni";
     private TextView tvShowResult;
+    private Spinner gpioList;
     private Button btChmodPort,btOpenSel,btCloseSel,btsetLightOn,btsetLightOff;
-
     public volatile boolean isOver=true;
+
 
 
     @Override
@@ -28,13 +33,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btCloseSel=(Button)findViewById(R.id.closePort);
         btsetLightOn=(Button)findViewById(R.id.setLightOn);
         btsetLightOff=(Button)findViewById(R.id.setLightOff);
+        gpioList=(Spinner)findViewById(R.id.gpioList);
 
         btOpenSel.setOnClickListener(this);
         btCloseSel.setOnClickListener(this);
         btsetLightOn.setOnClickListener(this);
         btsetLightOff.setOnClickListener(this);
         btChmodPort.setOnClickListener(this);
+
+
+        String[] str={"电源灯1-gpio0","电源灯2-gpio1","蓝牙-gpio2","USB电源-gpio3","升压使能-gpio4"};
+        ArrayAdapter adapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1,str);
+        gpioList.setAdapter(adapter);
     }
+
 
     @Override
     public void onClick(View v) {
@@ -42,11 +54,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             ioctlExec=new IoctlExec();
         }
         String str="";
+        String gpio="";
         final int flag;
         switch (v.getId()){
             case R.id.chmodPort:
                 isOver=false;
-                //开启一个线程，将shell命令送出
                 new Thread(new Runnable() {
                     @Override
                     public void run() {
@@ -66,19 +78,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
                 break;
             case R.id.setLightOn:
-                flag=ioctlExec.setLightOn();
+                gpio=gpioList.getSelectedItem().toString();
+                flag=ioctlExec.setLightOn(Integer.parseInt(gpio.substring(gpio.length()-1)));
+
                 if(flag==0){
                     str="设置灯亮成功";
-                }else{
+                }else if(flag==-1){
                     str="设置灯亮失败";
+                }else {
+                    str="请输入正确的gpio口";
                 }
                 break;
             case R.id.setLightOff:
-                flag=ioctlExec.setLightOff();
+                gpio=gpioList.getSelectedItem().toString();
+                flag=ioctlExec.setLightOff(Integer.parseInt(gpio.substring(gpio.length()-1)));
                 if(flag==0){
                     str="设置灯灭成功";
-                }else{
+                }else if(flag==-1){
                     str="设置灯灭失败";
+                }else {
+                    str="请输入正确的gpio口";
                 }
                 break;
             case R.id.closePort:
